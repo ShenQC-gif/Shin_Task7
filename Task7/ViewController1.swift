2e82bde66c6bd17be016ac72a082f065126ad8f9
QiChen <shen24qichen@gmail.com>
2021-05-31 07:52:59 +0900

    Initial Commit

---
+++ Task7/ViewController1.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Task7
+//
+//  Created by ちいつんしん on 2021/05/31.
+//
+
+import UIKit
+
+class ViewController1: UIViewController {
+
+    @IBOutlet private weak var textField1: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
+    @IBOutlet private weak var resultLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        textField1.keyboardType = .numberPad
+        textField2.keyboardType = .numberPad
+    }
+
+    @IBAction func didTapCalculateButton(_ sender: Any) {
+
+        let num1 = Int(textField1.text ?? "") ?? 0
+        let num2 = Int(textField2.text ?? "") ?? 0
+
+        resultLabel.text = "\(num1 + num2)"
+
+        textField1.endEditing(true)
+        textField2.endEditing(true)
+
+    }
+
+}
